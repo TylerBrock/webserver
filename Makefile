66970686f3e52e44485a40247c1fb25138cbb6ed
Tyler Brock <tyler.brock@gmail.com>
2016-01-03 03:30:48 -0500

    Add ability to dangerously serve files

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC=clang
-CFLAGS=-Wall -g
+CFLAGS=-Wall -g -O3
 
 all: webserver
 
--- webserver.c
+++ webserver.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,8 +10,9 @@
 
 static const int PORT = 3000;
 static const int BUF_SIZE = 1024 * 1024;
-static const int MAX_SIZE = 1024;
+static const int MAX_SIZE = 1024 * 1024;
 static const short HTTP_STATUS_OK = 200;
+static const short HTTP_STATUS_NOT_FOUND = 404;
 static const char* HTTP_VERSION = "HTTP/1.1";
 
 typedef struct {
@@ -32,14 +34,14 @@ typedef struct {
 
 request* new_request() {
   request* request_ptr = calloc(1, sizeof(request));
-  request_ptr->method = malloc(MAX_SIZE);
-  request_ptr->target = malloc(MAX_SIZE);
+  request_ptr->method = calloc(1, MAX_SIZE);
+  request_ptr->target = calloc(1, MAX_SIZE);
   return request_ptr;
 }
 
 response* new_response() {
   response* response_ptr = calloc(1, sizeof(response));
-  response_ptr->body = malloc(MAX_SIZE);
+  response_ptr->body = calloc(1, MAX_SIZE);
   return response_ptr;
 }
 
@@ -54,6 +56,21 @@ void destroy_response(response* response_ptr) {
   free(response_ptr);
 }
 
+void make_response(request* request_ptr, response* response_ptr) {
+  char relative_path[MAX_SIZE];
+  sprintf(relative_path, ".%s", request_ptr->target);
+  int file = open(relative_path, O_RDONLY);
+
+  if (file < 0) {
+    response_ptr->status = HTTP_STATUS_NOT_FOUND;
+  } else {
+    response_ptr->status = HTTP_STATUS_OK;
+    read(file, response_ptr->body, MAX_SIZE - 1);
+    close(file);
+  }
+  printf("[RESPONSE] %s %s %hd\n", request_ptr->method, request_ptr->target, response_ptr->status);
+}
+
 void parse_request(char* buffer, request* request_ptr) {
   sscanf(buffer, "%s %s %hd/%hd",
     request_ptr->method,
@@ -61,6 +78,7 @@ void parse_request(char* buffer, request* request_ptr) {
     &request_ptr->version_major,
     &request_ptr->version_minor
   );
+  printf("[REQUEST] %s %s\n", request_ptr->method, request_ptr->target);
 }
 
 void error(char *msg) {
@@ -70,7 +88,7 @@ void error(char *msg) {
 
 void send_response(int client_socket, response* response_ptr) {
   char buffer[2048];
-  sprintf(buffer, "%s %hd %s\r\n\r\n%s", HTTP_VERSION, response_ptr->status, "OK", response_ptr->body);
+  sprintf(buffer, "%s %hd \r\n\r\n%s", HTTP_VERSION, response_ptr->status, response_ptr->body);
   int n = write(client_socket, buffer, strnlen(buffer, 2048));
   if (n < 0) {
     error("Error writing to socket\n");
@@ -95,7 +113,10 @@ int make_server_socket() {
     error("Cannot bind to port");
   }
 
-  listen(sock, 5);
+  if (listen(sock, 5)) {
+    error("Unable to listen");
+  }
+
   return sock;
 }
 
@@ -111,7 +132,7 @@ void handle_request(int client_socket) {
   num_bytes = read(client_socket, buffer_ptr, BUF_SIZE - 1);
 
   if (num_bytes < 0) {
-    error("Error reading from socket\n");
+    error("Error reading from socket");
   }
 
   buffer_ptr[num_bytes] = 0;
@@ -119,14 +140,14 @@ void handle_request(int client_socket) {
   request* request_ptr = new_request();
   parse_request(buffer_ptr, request_ptr);
   free(buffer_ptr);
-  printf("[REQUEST] %s %s\n", request_ptr->method, request_ptr->target);
 
+  // Build response
   response* response_ptr = new_response();
-  response_ptr->status = HTTP_STATUS_OK;
-  sprintf(response_ptr->body, "Request was Method:%s Target:%s", request_ptr->method, request_ptr->target);
-  destroy_request(request_ptr);
+  make_response(request_ptr, response_ptr);
   send_response(client_socket, response_ptr);
+
   close(client_socket);
+  destroy_request(request_ptr);
   destroy_response(response_ptr);
 }
 
