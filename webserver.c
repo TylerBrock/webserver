4a13b68168783a223785f661fb998de74a6f7170
Tyler Brock <tyler.brock@gmail.com>
2016-01-04 22:17:03 -0800

    Add signal handler for SIGINT

--- webserver.c
+++ webserver.c
@@ -1,22 +1,28 @@
+#include <errno.h>
 #include <fcntl.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 
-static const int PORT = 3000;
+static const int DEFAULT_PORT = 3000;
+static const int BACKLOG = 5;
 static const int BUF_SIZE = 1024 * 1024;
 static const int MAX_SIZE = 1024 * 1024;
 
 typedef enum { OK, NOT_FOUND, ERROR } http_status;
 static const char* HTTP_STATUS_STRING[] = { "OK", "Not Found", "Internal Server Error" };
 static const short HTTP_STATUS_CODE[] = { 200, 404, 500 };
-
 static const char* HTTP_VERSION = "HTTP/1.1";
+
+volatile sig_atomic_t stop = false;
 
 typedef struct {
   char* key;
@@ -34,6 +40,12 @@ typedef struct {
   http_status status;
   char* body;
 } response;
+
+void sig_handler(int signo) {
+  if (signo == SIGINT) {
+    stop = true;
+  }
+}
 
 request* new_request() {
   request* request_ptr = calloc(1, sizeof(request));
@@ -65,9 +77,9 @@ void make_response(request* request_ptr, response* response_ptr) {
   int file = open(relative_path, O_RDONLY);
 
   if (file < 0) {
-    response_ptr->status = HTTP_STATUS_CODE[NOT_FOUND];
+    response_ptr->status = NOT_FOUND;
   } else {
-    response_ptr->status = HTTP_STATUS_CODE[OK];
+    response_ptr->status = OK;
     read(file, response_ptr->body, MAX_SIZE - 1);
     close(file);
   }
@@ -102,7 +114,7 @@ void send_response(int client_socket, response* response_ptr) {
 }
 
 int make_server_socket() {
-  int sock;
+  int sock, reuse, result;
   struct sockaddr_in serv_addr;
   sock = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -110,16 +122,25 @@ int make_server_socket() {
     error("Error opening socket");
   }
 
+  reuse = true;
+  result = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+  if (result < 0) {
+    error("Error setting socket options");
+  }
+
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
-  serv_addr.sin_port = htons(PORT);
+  serv_addr.sin_port = htons(DEFAULT_PORT);
 
-  if (bind(sock, (struct sockaddr *) (&serv_addr), sizeof(serv_addr)) < 0) {
+  result = bind(sock, (struct sockaddr *) (&serv_addr), sizeof(serv_addr));
+  if (result < 0) {
     error("Cannot bind to port");
   }
 
-  if (listen(sock, 5)) {
+  result = listen(sock, BACKLOG);
+  if (result < 0) {
     error("Unable to listen");
   }
 
@@ -159,11 +180,30 @@ void handle_request(int client_socket) {
 
 void run_server(int server_socket) {
   socklen_t client_addr_len;
-  int client_socket;
+  int client_socket, result;
   struct sockaddr_in cli_addr;
   client_addr_len = sizeof(cli_addr);
 
-  while(1) {
+  while(!stop) {
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(server_socket, &fds);
+
+    //TODO: Consider using pselect to avoid race condition
+    result = select(server_socket + 1, &fds, NULL, NULL, NULL);
+
+    if (result < 0 && errno != EINTR) {
+      error("Error in select");
+    }
+
+    if (stop) {
+      break;
+    }
+
+    if (!result) {
+      continue;
+    }
+
     client_socket = accept(server_socket, (struct sockaddr *) &cli_addr, &client_addr_len);
 
     if (client_socket < 0) {
@@ -177,10 +217,10 @@ void run_server(int server_socket) {
 int main(int argc, char* argv[]) {
   int server_socket;
 
+  signal(SIGINT, sig_handler);
   server_socket = make_server_socket();
   run_server(server_socket);
-  // TODO: close server socket in signal handler
-  //close(server_socket);
+  close(server_socket);
 
   return EXIT_SUCCESS;
 }
